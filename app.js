927a7d219d72bffac35fc6c2ed55591fc4467a20
Sean Baillargeon <seanpbaillargeon@gmail.com>
2016-02-18 16:38:43 -0700

    need to link rating button

--- app.js
+++ app.js
@@ -12,12 +12,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(logger('dev'))
 
+var listItem = function(obj){
+		this.name = obj.name, 
+		this.url = obj.url, 
+		this.title = obj.title, 
+		this.description = obj.description, 
+		this.rating = obj.rating
+}
+
 var list = [
 	{
 		name	: 'Tom Cat',
 		url 	: "https://www.youtube.com/embed/SrXAaWyDWOY",
 		title	: 'It\'s friday!!',
-		description : 'This is what I do in my free time.'
+		description : 'This is what I do in my free time.',
+		rating	: 0
 	}
 ]
 
@@ -30,11 +39,35 @@ app.get('/', function(req, res){
 app.get('/api/list', function(req, res){
 	res.send(list)
 })
+app.post('/api/list', function(req, res){
+	if(list.length < 8){
+		var item = new listItem(req.body)
+		list.push(item)
+		res.send(list)
+	}
+	else {
+		res.send('Too Many Videos Brah!!!!!!!')
+	}
+})
 // Creating Server and Listening for Connections \\
 var port = 3000
 app.listen(port, function(){
   console.log('Server running on port ' + port);
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- public/main.js
+++ public/main.js
@@ -2,10 +2,23 @@ angular.module('moduleOne',[])
 
 angular.module('moduleOne')
 	.controller('controllerOne', ['$scope', '$http', '$sce', function($scope, $http, $sce){
-
+		$scope.$sce = $sce
+		
 		$http.get('/api/list')
 			 .then(function(serverData){
 			 	$scope.list = serverData.data
 			 })
 
+		$scope.newVideo = function(){
+			$http.post('/api/list', $scope.sub)
+				 .then(function(serverData){
+				 	$scope.list = serverData.data
+				 	$scope.sub = {}
+				 })				 
+		}
+
+		$scope.liked = function(){
+
+			$http.post('/api/rate')
+		}
 	}])
