3f0e1e2c206218dbdc8b3e13d38fcd24407d482a
Sean Baillargeon <seanpbaillargeon@gmail.com>
2016-02-19 13:20:01 -0700

    bootstrap displays only 2

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ angular.module('moduleOne',[])
 angular.module('moduleOne')
 	.controller('controllerOne', ['$scope', '$http', '$sce', function($scope, $http, $sce){
 		$scope.$sce = $sce
-		
+		$scope.displayNumber = 0
 		$http.get('/api/list')
 			 .then(function(serverData){
 			 	$scope.list = serverData.data
@@ -23,11 +23,25 @@ angular.module('moduleOne')
 				 	console.log(serverData)
 				 	$scope.list.forEach(function(video){
 				 		if(video.name == serverData.data.name){
-				 			video.rating = serverData.data.num
-				 			
+				 			video.rating = serverData.data.num	
 				 		}
-				 	})
-				 	
+				 	})				 	
 				 })
 		}
+		$scope.increase = function(){
+	 		if ($scope.displayNumber>=8){
+	 			$scope.displayNumber = 8
+	 		}
+	 		else {
+	 		$scope.displayNumber+=2
+	 		}
+ 		}
+ 		$scope.decrease = function(){
+	 		if ($scope.displayNumber<0){
+	 			$scope.displayNumber = 0
+	 		}
+	 		else {
+	 			$scope.displayNumber-=2
+	 		}
+ 		}
 	}])
